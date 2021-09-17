ce276eecb0d1d2ddaabb1f56d5d89c3b5a45cedd
aman-can <aman.dubey1998@gmail.com>
2021-09-17 17:55:13 +0530

    Made it responsive

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,33 +1,48 @@
 import {
-    List,
     Drawer,
     Typography,
+    List,
+    IconButton,
+    Hidden,
     ListItem,
     ListItemIcon,
     ListItemText,
     AppBar,
-    Toolbar,
     Avatar,
+    Toolbar,
+    Divider,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
-import { AddCircleOutlineRounded, SubjectRounded } from "@material-ui/icons";
-import React from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import { makeStyles, useTheme } from "@material-ui/core";
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router";
+import {
+    AddCircleOutlineRounded,
+    SubjectRounded,
+    MenuRounded,
+} from "@material-ui/icons";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => {
     return {
         page: {
-            background: "#f9f9f9",
             width: "100%",
+            flexGrow: 1,
             padding: theme.spacing(3),
+            [theme.breakpoints.up("sm")]: {
+                width: `calc(100% - ${drawerWidth}px)`,
+                marginLeft: drawerWidth,
+            },
         },
+
         root: {
             display: "flex",
         },
         drawer: {
-            width: drawerWidth,
+            [theme.breakpoints.up("sm")]: {
+                width: drawerWidth,
+                flexShrink: 0,
+            },
         },
         drawerPaper: {
             width: drawerWidth,
@@ -39,7 +54,16 @@ const useStyles = makeStyles((theme) => {
             padding: theme.spacing(2),
         },
         appBar: {
-            width: `calc(100% - ${drawerWidth}px)`,
+            [theme.breakpoints.up("sm")]: {
+                width: `calc(100% - ${drawerWidth}px)`,
+                marginLeft: drawerWidth,
+            },
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+            [theme.breakpoints.up("sm")]: {
+                display: "none",
+            },
         },
         toolBar: theme.mixins.toolbar,
         date: {
@@ -50,11 +74,19 @@ const useStyles = makeStyles((theme) => {
         },
     };
 });
+
 function Layout({ children }) {
-    const classes = useStyles();
     const history = useHistory();
     const location = useLocation();
     const date = new Date();
+    const classes = useStyles();
+    const theme = useTheme();
+
+    const [mobileOpen, setMobileOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
     const menuList = [
         {
@@ -69,16 +101,52 @@ function Layout({ children }) {
         },
     ];
 
+    const drawer = (
+        <div>
+            <div>
+                <Typography className={classes.title} variant="h6">
+                    {`Today is ${date.getDate()} - ${
+                        date.getMonth() + 1
+                    } - ${date.getFullYear()}`}
+                </Typography>
+            </div>
+            <Divider />
+            <List>
+                {menuList.map((item) => (
+                    <ListItem
+                        button
+                        key={item.path}
+                        onClick={() => {
+                            history.push(item.path);
+                        }}
+                        className={
+                            location.pathname === item.path
+                                ? classes.active
+                                : null
+                        }
+                    >
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
     return (
         <div className={classes.root}>
-            {/* App bar */}
-
-            <AppBar className={classes.appBar} elevation={0}>
+            <AppBar className={classes.appBar}>
                 <Toolbar>
-                    <Typography className={classes.date}>
-                        {`Today is ${date.getDate()} - ${
-                            date.getMonth() + 1
-                        } - ${date.getFullYear()}`}
+                    <IconButton
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        color="inherit"
+                        className={classes.menuButton}
+                    >
+                        <MenuRounded />
+                    </IconButton>
+                    <Typography className={classes.date} noWrap variant="h6">
+                        Your Notes
                     </Typography>
                     <Typography>aman</Typography>
                     <Avatar
@@ -89,43 +157,29 @@ function Layout({ children }) {
                     />
                 </Toolbar>
             </AppBar>
-            {/* Side Drawer */}
-            <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                classes={{ paper: classes.drawerPaper }}
-                anchor="left"
-            >
-                <div>
-                    <Typography className={classes.title} variant="h5">
-                        Your Notes
-                    </Typography>
-                </div>
-                {/* List/Link */}
-
-                <List>
-                    {menuList.map((item) => (
-                        <ListItem
-                            button
-                            key={item.path}
-                            onClick={() => {
-                                history.push(item.path);
-                            }}
-                            className={
-                                location.pathname === item.path
-                                    ? classes.active
-                                    : null
-                            }
-                        >
-                            <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText primary={item.text} />
-                        </ListItem>
-                    ))}
-                </List>
-            </Drawer>
-
+            <nav>
+                <Hidden smUp implementation="css">
+                    <Drawer
+                        variant="temporary"
+                        anchor={theme.direction === "rtl" ? "right" : "left"}
+                        open={mobileOpen}
+                        onClose={handleDrawerToggle}
+                        classes={{ paper: classes.drawerPaper }}
+                    >
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer
+                        classes={{ paper: classes.drawerPaper }}
+                        variant="permanent"
+                    >
+                        {drawer}
+                    </Drawer>
+                </Hidden>
+            </nav>
             <div className={classes.page}>
-                <div className={classes.toolBar}></div>
+                <div className={classes.toolBar} />
                 {children}
             </div>
         </div>
